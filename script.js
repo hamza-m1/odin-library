df9d36ffee90cf674cf1192e3b2f20c655341a09
Hamza <177459013+hamza-m1@users.noreply.github.com>
2025-02-21 18:38:22 +0000

    Add function that adds books to library

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function Book(name, author) {
     this.name = name
     this.author = author
 }
+
+function addBookToLibrary(name, author) {
+    myLibrary.push(new Book(name, author))
+}
+
+
+// addBookToLibrary('duckling', 'james')
+// console.log(myLibrary)
