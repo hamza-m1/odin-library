e1f46306147fbe32ee47e93dfffc637284e17278
Hamza <177459013+hamza-m1@users.noreply.github.com>
2025-03-11 22:53:24 +0000

    Create displayBooks function

--- script.js
+++ script.js
@@ -15,12 +15,50 @@ addBookToLibrary('duckling', 'James', 30, true)
 addBookToLibrary('cat', 'Richard', 67, true)
 addBookToLibrary('frog on a log', 'Harper', 119, false)
 
+const bookContainer = document.querySelector('.books-container')
+
 function displayBooks() {
-    
+    myLibrary.map((item, index, array) => {
+        const newBookCard = document.createElement('div')
+        console.log(newBookCard)
+
+        // const removeSpaces = item.name.replace(/\s+/g, '-').toLowerCase()
+        // `${removeSpaces}-card`
+        newBookCard.classList.add('book-card')
+        newBookCard.textContent = 'from the map'
+        bookContainer.appendChild(newBookCard)
+        return item
+    })
+
+
+    // for(let i=1; i<=myLibrary.length; i++) {
+    //     const newBookCard = document.createElement('div')
+    //     // console.log(newBookCard)
+
+    //     // const removeSpaces = item.name.replace(/\s+/g, '-').toLowerCase()
+    //     // `${removeSpaces}-card`
+    //     newBookCard.classList.add('book-card')
+    //     newBookCard.textContent = 'from the map'
+    //     bookContainer.appendChild(newBookCard)
+    // }
 }
 
 
 
 
 
-console.log(myLibrary)
+
+
+// console.log(myLibrary)
+displayBooks()
+
+
+
+
+
+
+
+    // const name = document.createElement('p')
+    // name.textContent = item.name
+    // name.appendChild(newBookCard)
+    // console.log(item.name)
