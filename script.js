66aac43ea4994b4c12d43ca3e6324982f645f8a2
Hamza <177459013+hamza-m1@users.noreply.github.com>
2025-03-20 23:31:52 +0000

    Clean up code

--- script.js
+++ script.js
@@ -12,26 +12,17 @@ function addBookToLibrary(name, author, pages, haveRead) {
     myLibrary.push(new Book(name, author, pages, haveRead))
 }
 
-
-
 addBookToLibrary('duckling', 'James', 30, true)
 addBookToLibrary('cat', 'Richard', 67, true)
 addBookToLibrary('frog on a log', 'Harper', 119, false)
 
 const bookContainer = document.querySelector('.books-container')
-// const addBookBtn = document.getElementById('add-book-btn')
 const dialog = document.querySelector('dialog')
 const closeBtn = document.getElementById('close-btn')
 const confirmBtn = document.getElementById('confirm-btn')
 const form = document.getElementById('form')
 
-// const allBookCards = document.querySelectorAll('[data-id]')
-// const cardCloseBtns = document.querySelectorAll('[data-btn-id]')
-
-// const allBookCards = document.querySelectorAll('.book-card')
-
 function displayBooks() {
-    // const allBookCards = document.querySelectorAll('.book-card')
     bookContainer.innerHTML = ''
 
     const addBookCard = document.createElement('div')
@@ -44,61 +35,48 @@ function displayBooks() {
 
 
     myLibrary.map((item, index, array) => {
-        // let check = false
-        // for(let i=0; i<allBookCards.length; i++) {
-        //     if (allBookCards[i].firstChild.firstChild.data === item.name) {
-        //         check = true
-        //     }
-        // }
-        // if (check === false) {
-        // }  
         const newBookCard = document.createElement('div')
-            newBookCard.classList.add('book-card', `${item.name.replace(/\s+/g, '-').toLowerCase()}`)
-            newBookCard.setAttribute('data-id', `${item.name}`)
+        newBookCard.classList.add('book-card', `${item.name.replace(/\s+/g, '-').toLowerCase()}`)
+        newBookCard.setAttribute('data-id', `${item.name}`)
 
-            const closeBtn = document.createElement('button')
-            closeBtn.classList.add('card-close-btn')
-            closeBtn.textContent = 'x'
-            closeBtn.setAttribute('data-Btnid', `${item.name}`)
+        const closeBtn = document.createElement('button')
+        closeBtn.classList.add('card-close-btn')
+        closeBtn.textContent = 'x'
+        closeBtn.setAttribute('data-Btnid', `${item.name}`)
 
+        const name = document.createElement('p')
+        name.classList.add('name')
+        name.textContent = `${item.name}`
 
-            const name = document.createElement('p')
-            name.classList.add('name')
-            name.textContent = `${item.name}`
+        const author = document.createElement('p')
+        author.classList.add('author')
+        author.textContent = `by ${item.author}`
 
-            const author = document.createElement('p')
-            author.classList.add('author')
-            author.textContent = `by ${item.author}`
+        const pages = document.createElement('p')
+        pages.classList.add('pages')
+        pages.textContent = `${item.pages} pages`
 
-            const pages = document.createElement('p')
-            pages.classList.add('pages')
-            pages.textContent = `${item.pages} pages`
+        const haveRead = document.createElement('button')
+        haveRead.classList.add('have-read')
+        if (item.haveRead) {
+            haveRead.textContent = `read`
+        } else {
+            haveRead.textContent = `not read`
+        }
+        haveRead.setAttribute('data-readbtnid', `${item.name}`)
+        haveRead.setAttribute('haveRead', `${item.haveRead}`)
 
-            const haveRead = document.createElement('button')
-            haveRead.classList.add('have-read')
-            if (item.haveRead) {
-                haveRead.textContent = `read`
-            } else {
-                haveRead.textContent = `not read`
-            }
-            haveRead.setAttribute('data-readbtnid', `${item.name}`)
-            haveRead.setAttribute('haveRead', `${item.haveRead}`)
-
-            newBookCard.appendChild(closeBtn)
-            newBookCard.appendChild(name)
-            newBookCard.appendChild(author)
-            newBookCard.appendChild(pages)
-            newBookCard.appendChild(haveRead)
-            bookContainer.appendChild(newBookCard)
+        newBookCard.appendChild(closeBtn)
+        newBookCard.appendChild(name)
+        newBookCard.appendChild(author)
+        newBookCard.appendChild(pages)
+        newBookCard.appendChild(haveRead)
+        bookContainer.appendChild(newBookCard)
     })
-    // let addBookBtn = document.getElementById('add-book-btn')
-    // console.log(allBookCards)
     resetAddBookBtn()
     resetCardCloseBtn()
     resetHaveReadBtn()
 }
-
-displayBooks()
 
 const allBookCards = document.querySelectorAll('[data-id]')
 const cardCloseBtns = document.querySelectorAll('[data-BtnId]')
@@ -138,11 +116,11 @@ function resetHaveReadBtn() {
     })
 }
 
-
 closeBtn.addEventListener('click', (e) => {
     const name = document.getElementById('name')
     const author = document.getElementById('author')
     const pages = document.getElementById('pages')
+    
     dialog.close()
     e.preventDefault()
     name.value = ''
@@ -164,51 +142,8 @@ confirmBtn.addEventListener('click', (e) => {
         author.value = ''
         pages.value = ''
         dialog.close(formData)
-        // console.log(dialog.returnValue)
         displayBooks()
     }
 })
 
-// cardCloseBtns.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         // console.log(btn.dataset.btnid)
-//         myLibrary.map((item, index, array) => {
-//             if (item.id === btn.dataset.btnid) {
-//                 console.log(item.id)
-//                 myLibrary.splice(index, 1)
-//             }
-//         })
-//         displayBooks()
-//     })
-// })
-
-// console.log(cardCloseBtns)
-
-
-
-// console.log(myLibrary)
-// displayBooks()
-
-
-
-
-
-
-
-    // const name = document.createElement('p')
-    // name.textContent = item.name
-    // name.appendChild(newBookCard)
-    // console.log(item.name)
-
-
-
-    // for(let i=1; i<=myLibrary.length; i++) {
-    //     const newBookCard = document.createElement('div')
-    //     // console.log(newBookCard)
-
-    //     // const removeSpaces = item.name.replace(/\s+/g, '-').toLowerCase()
-    //     // `${removeSpaces}-card`
-    //     newBookCard.classList.add('book-card')
-    //     newBookCard.textContent = 'from the map'
-    //     bookContainer.appendChild(newBookCard)
-    // }
+displayBooks()
